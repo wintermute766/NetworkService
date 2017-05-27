596b4088a7126b6c5286632b90e36ef40cf33117
Eugene K <player766@gmail.com>
2017-05-27 14:40:10 +0200

    temp0

--- app/src/main/java/ru/sberbank/learning/networkservice/DownloadService.java
+++ app/src/main/java/ru/sberbank/learning/networkservice/DownloadService.java
@@ -2,15 +2,82 @@ package ru.sberbank.learning.networkservice;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DownloadService extends IntentService {
+
+    public static final String EXTRA_FILE_NAME = "file_name";
+
+    private boolean completed = false;
+    private boolean withErrors = false;
+    private int progress = 0;
 
     public DownloadService() {
         super("DownloadService");
     }
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new LocalBinder();
+    }
+
     @Override
     protected void onHandleIntent(Intent intent) {
 
+
+
+    }
+
+    private void downloadFile(URL url, String fileName) throws IOException {
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        int size = connection.getContentLength();
+
+        File directory =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        directory.mkdirs();
+        File file = new File(directory, fileName);
+
+        BufferedInputStream is = new BufferedInputStream(connection.getInputStream());
+        BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(file));
+
+        byte[] buffer = new byte[8096];
+        int readed;
+
+        while ((readed = is.read(buffer)) > 0) {
+            os.write(buffer, 0, readed);
+        }
+
+        os.flush();
+        os.close();
+
+        connection.disconnect();
+    }
+
+    public class LocalBinder extends Binder {
+
+        public boolean isCompleted() {
+            return completed;
+        }
+
+        public boolean hasErrors() {
+            return withErrors;
+        }
+
+        public int getProgress() {
+            return progress;
+        }
     }
 }
--- app/src/main/java/ru/sberbank/learning/networkservice/MainActivity.java
+++ app/src/main/java/ru/sberbank/learning/networkservice/MainActivity.java
@@ -1,13 +1,65 @@
 package ru.sberbank.learning.networkservice;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
+
+    private DownloadService.LocalBinder binder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            binder = (DownloadService.LocalBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            binder = null;
+        }
+    };
+
+    private View downloadButton;
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloadButton = findViewById(R.id.button_download);
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent service = new Intent(MainActivity.this, DownloadService.class);
+                service.setData(Uri.parse("http://droider.ru/wp-content/uploads/2017/03/AndroidPIT-android-O-Oreo-2083.jpg"));
+                service.putExtra(DownloadService.EXTRA_FILE_NAME, "android.jpg");
+                startService(service);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent service = new Intent(this, DownloadService.class);
+        bindService(service, connection, 0);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (binder != null) {
+            unbindService(connection);
+        }
     }
 }
